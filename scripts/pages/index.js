a28bbb1c17109abf20f6b6088c250f29bdc1d198
brice766 <briere.brice@gmail.com>
2023-04-11 21:45:36 +0200

    st

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,35 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+    const respons = await fetch("data/photographers.json");
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    let data = await respons.json();
+    let photographers = [...data.photographers];
+    let dataMedia = [...data.media];
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+    /*création de photographerMedias qui contient les oeuvres de chacun des photographes
+    et on l'ajoute dnas l'ARRAY qui contient les informations personnelles de chacun d'eux
+    le résultat est :photographers */
+    photographers.forEach(photographer => {
+        const photographerMedias = dataMedia.filter(photographerMedia => photographerMedia.photographerId === photographer.id);
+        photographer.medias = [...photographerMedias];
+    })
+    // et bien retourner le tableau photographers seulement une fois
+    return { photographers }
+}
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
